ff500ff9126074836f9fa3c63ccb4533bc53f5ea
Rohit Yengantiwar <yengantiwarrohit1239@gmail.com>
2026-06-19 22:32:35 +0530

    Switch database backend to SQLite

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -120,12 +120,8 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": env("DATABASE_HOST"),
-        "PORT": env("DATABASE_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
